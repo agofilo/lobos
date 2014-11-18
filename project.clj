1451ed069e32e291b005f6875b6ec6748fc075a9
siscia <simone@mweb.biz>
2014-11-18 17:05:05 +0100

    change project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lobos "1.0.0-beta3"
+(defproject lobos "siscia/1.0.0-quick-dirt"
   :description
   "A library to create and manipulate SQL database schemas."
   :url "http://budu.github.com/lobos/"
